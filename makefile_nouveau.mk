3920d03aeeefefbe1a1cac51fb231ebbbcff5d8a
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-01-17 22:11:53 -0500

    Section comments

--- makefile_nouveau.mk
+++ makefile_nouveau.mk
@@ -11,6 +11,7 @@ PROJECTNAME=test
 PRJSRC=$(wildcard *.cpp)
 MCU=atmega324pa
 
+######################### Variables pour la compilation ########################
 CC=avr-gcc
 INC=-I.
 OPTLEVEL=s
@@ -19,20 +20,24 @@ MISC_FLAGS=-fpack-struct -fshort-enums -funsigned-bitfields -funsigned-char
 CFLAGS=-MMD $(INC) -g -mmcu=$(MCU) -O$(OPTLEVEL) $(WARNING_FLAGS) $(MISC_FLAGS)
 CXXFLAGS=-fno-exceptions
 
+############################# Variables pour le linking ########################
 LD=avr-gcc
 LIBS=
 LDFLAGS=-Wl,-Map,$(TRG).map -mmcu=$(MCU)
 TRG=$(PROJECTNAME).out
 
+####################### Faire la liste des fichiers objets #####################
 CFILES=$(filter %.c, $(PRJSRC))
 CPPFILES=$(filter %.cpp, $(PRJSRC))
 OBJDEPS=$(CFILES:.c=.o) $(CPPFILES:.cpp=.o)
 
+########################### Settings pour avr-objcopy ##########################
 OBJCOPY=avr-objcopy
 HEXFORMAT=ihex
 HEXROMTRG=$(PROJECTNAME).hex
 HEXTRG=$(HEXROMTRG) $(PROJECTNAME).ee.hex
 
+########################### Settings pour make install #########################
 AVRDUDE=avrdude
 AVRDUDE_PROGRAMMERID=usbasp
 
